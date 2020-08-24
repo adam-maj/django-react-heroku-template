0689db4a12ebf61cb6a886e23a2b08ee0f61590d
adam-maj <mr.adam.maj@gmail.com>
2020-08-24 15:53:26 -0400

    Configure React frontend and DRF API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class APIOverview(APIView):
+    api_urls = {
+        'View Name': 'View URL Slug' 
+    }
+
+    def get(self, request, format=None):
+        return Response(self.api_urls, status=status.HTTP_200_OK)
